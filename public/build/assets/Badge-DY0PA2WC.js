22817c76a56331d57905bec0e1def1b49a6bebd5
JayraldBernales07 <Bernalesj28@gmail.com>
2025-05-11 13:00:14 +0800

    Rebuild frontend and update build folder

--- public/build/assets/Badge-DY0PA2WC.js
+++
@@ -1,1 +0,0 @@
-import{u as i,c as d}from"./divWithClassName-DRtHi257.js";import{r as x,j as c}from"./app-CQPZVfaW.js";const l=x.forwardRef(({bsPrefix:r,bg:s="primary",pill:e=!1,text:a,className:o,as:t="span",...p},m)=>{const f=i(r,"badge");return c.jsx(t,{ref:m,...p,className:d(o,f,e&&"rounded-pill",a&&`text-${a}`,s&&`bg-${s}`)})});l.displayName="Badge";export{l as B};
